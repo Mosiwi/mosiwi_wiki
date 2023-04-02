2ec35a9858978f627b15f9ad0efa141f7895f261
“mosiwi” <1123552240@qq.com>
2023-04-02 14:50:49 +0800

    init

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,6 +36,10 @@ html_search_language = 'en'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 html_static_path = ['_static']
 html_logo = '_static/web_logo/logo_mosiwi.png'
