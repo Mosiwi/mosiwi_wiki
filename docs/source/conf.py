60b2aa30c17bb94199f07e8a0c694dd168a316ce
“mosiwi” <1123552240@qq.com>
2023-04-02 12:11:10 +0800

    init

---
+++ docs/source/conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import datetime
+current_year = datetime.datetime.now().year
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Mosiwi Wiki'
+copyright = '2022 - {}, Shenzhen Mosiwi Technology Co., Ltd'.format(current_year)
+author = 'Mosiwi'
+release = 'v1.0'
+
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'myst_parser'
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Language to be used for generating the HTML full-text search index.
+# Sphinx supports the following languages:
+#   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
+#   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr', 'zh'
+#
+html_search_language = 'en'
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_logo = '_static/web_logo/logo_mosiwi.png'
