9098832fdcbcd7e4ce6361f59fdfea06de7f8496
“mosiwi” <1123552240@qq.com>
2023-10-22 17:24:59 +0800

    init

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,9 +21,9 @@ release = 'v1.0'
 extensions = [
     # https://myst-parser.readthedocs.io/en/latest/intro.html
     "myst_parser",         
-    #"sphinx_design",
-    #"sphinx_copybutton",   # Add a copy button to your code blocks
-    # "sphinx_tippy",      # Add tooltips to your documentation
+    "sphinx_design",
+    "sphinx_copybutton",   # Add a copy button to your code blocks
+    "sphinx_tippy",      # Add tooltips to your documentation
 ]
 
 source_suffix = {
@@ -34,8 +34,8 @@ source_parsers = {'.md': 'recommonmark.parser.CommonMarkParser'}
 
 # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#syntax-extensions
 myst_enable_extensions = [
-    #"colon_fence",
-    #"dollarmath",        # Displays mathematical formulas
+    "colon_fence",
+    "dollarmath",        # Displays mathematical formulas
 ]
 
 templates_path = ['_templates']
@@ -56,9 +56,10 @@ html_theme = 'sphinx_rtd_theme'
 # html_theme = "sphinx_book_theme"     # Another nice theme
 
 html_static_path = ['_static']
+
 # set font
 html_css_files = [
-    '_static/css_style/my_css-style.css',
+    #'_static/css_style/my_css-style.css',
 ]
 
 html_logo = '_static/web_logo/logo_mosiwi.png'
